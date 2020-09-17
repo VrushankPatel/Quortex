aa3ae4a64f10431258b2945a33bbb96914f8e35b
Vrushank Patel <34762923+VrushankPatel@users.noreply.github.com>
2020-09-17 17:42:42 +0530

    bug fixes

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         // proxy: 'http://localhost:9090',
         proxy: 'http://questaapp.herokuapp.com',
         disableHostCheck: true,
-        port: 5000,
+        port: 8080,
         host: '0.0.0.0'
     }
 }
