c442cdec4f37e051afdde0b40ff830872f8eccc3
Vrushank Patel <34762923+VrushankPatel@users.noreply.github.com>
2020-09-27 19:06:55 +0530

    changes in drawer

--- src/components/HomePageDrawer.vue
+++ src/components/HomePageDrawer.vue
@@ -1,43 +1,63 @@
 <template>
-  <div>
-    <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+	<div>
+		<md-toolbar
+			class="md-transparent"
+			md-elevation="0"
+			style="padding-left:20%;"
+		>
+			<span class="android-title mdl-layout-title">
+				<img
+					style="height:37px"
+					class="android-logo-image"
+					src="@/assets/images/android-logo.png"
+				/>
+			</span>
+		</md-toolbar>
 
-    <md-list>
-      <md-list-item>
-        <md-icon>move_to_inbox</md-icon>
-        <span class="md-list-item-text">Inbox</span>
-      </md-list-item>
-
-      <md-list-item>
-        <md-icon>send</md-icon>
-        <span class="md-list-item-text">Sent Mail</span>
-      </md-list-item>
-
-      <md-list-item>
-        <md-icon>delete</md-icon>
-        <span class="md-list-item-text">Trash</span>
-      </md-list-item>
-
-      <md-list-item>
-        <md-icon>error</md-icon>
-        <span class="md-list-item-text">Spam</span>
-      </md-list-item>
-    </md-list>
-  </div>
+		<md-divider></md-divider>
+		<md-list style="padding-top:20%">
+			<button id="btn-drawer" @click="editprofile">
+				<md-list-item>
+					<md-icon>edit</md-icon>
+					<span class="md-list-item-text">Edit profile</span>
+				</md-list-item>
+			</button>
+			<button id="btn-drawer" @click="logout">
+				<md-list-item>
+					<md-icon>login</md-icon>
+					<span class="md-list-item-text">Logout</span>
+				</md-list-item>
+			</button>
+		</md-list>
+	</div>
 </template>
 
 <script>
+import actions from "@/common/actions.js";
 export default {
-  data: () => ({
-    menuVisible: false,
-  }),
+	data: () => ({
+		menuVisible: false,
+	}),
+	methods: {
+		editprofile() {
+			this.$emit("editprofile");
+		},
+		logout() {
+			actions.fireLoggedOut(this.$swal, this.$router);
+		},
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../styles/questioncard.css";
 .md-drawer {
-  width: 230px;
-  max-width: calc(100vw - 125px);
+	width: 230px;
+	max-width: calc(100vw - 125px);
+}
+#btn-drawer {
+	border: none;
+	background-color: white;
+	cursor: pointer;
 }
 </style>
