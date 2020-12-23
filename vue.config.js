1658e399c128db795896481545519b3ad69a3ddd
Vrushank Patel <34762923+VrushankPatel@users.noreply.github.com>
2020-12-23 12:22:09 +0530

    commented the code in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-	devServer: {
-		proxy: "http://questaapp.herokuapp.com/api",
-		disableHostCheck: true,
-		port: 8080,
-		host: "0.0.0.0",
-		// https: true,
-	},
-	// outputDir: "target/dist",
-	// assetsDir: "static",
-};
+// module.exports = {
+// 	devServer: {
+// 		proxy: "http://questaapp.herokuapp.com/api",
+// 		disableHostCheck: true,
+// 		port: 8080,
+// 		host: "0.0.0.0",
+// 		// https: true,
+// 	},
+// 	// outputDir: "target/dist",
+// 	// assetsDir: "static",
+// };
