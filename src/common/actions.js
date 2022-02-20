7acefbcb8808909a82569aea6b84f896832b934e
Vrushank Patel <vrushankpatel5@gmail.com>
2022-02-20 10:16:52 +0530

    sonar fixes actions

--- src/common/actions.js
+++ src/common/actions.js
@@ -2,7 +2,7 @@ import cryptoUtil from "@/common/cryptoUtil.js";
 import constants from "@/common/constants.js";
 export default {
 	actionSignup(swal, code, status, router) {
-		if (status == false && code == 531) {
+		if (!status && code === 531) {
 			swal.fire({
 				icon: "error",
 				title: "Oops..",
@@ -16,31 +16,12 @@ export default {
 			text: "Account successfully created, \nPlease signin to continue.",
 		});
 		router.push("/Signin");
-	},
-	// successSignup(swal, code, status, router) {
-	// 	if (status == true && code == 200) {
-	// 		swal.fire({
-	// 			icon: "success",
-	// 			title: "Success",
-	// 			text: "Account successfully created, \nPlease signin to continue.",
-	// 		});
-	// 		router.push("/Signin");
-	// 	}
-	// },
-	// errorSignup(swal, code, status) {
-	// 	if (status == false && code == 531) {
-	// 		swal.fire({
-	// 			icon: "error",
-	// 			title: "Oops..",
-	// 			text: "Email Id already exists, please try again with different email.",
-	// 		});
-	// 	}
-	// },
+	},	
 	actionSignin(swal, code, status, router) {
-		if (status == true && code == 200) {
+		if (status && code === 200) {
 			router.push("/home");
 		}
-		if (status == false && code == 532) {
+		if (!status && code === 532) {
 			swal.fire({
 				icon: "error",
 				title: "Oops..",
@@ -49,13 +30,13 @@ export default {
 		}
 	},
 	successQuestionPost(code, status) {
-		if (status == true && code == 200) {
+		if (status && code === 200) {
 			return true;
 		}
 		return false;
 	},
 	errorQuestionPost(code, status, router, swal) {
-		if (!this.checkLoggedIn(router) || code == 555) {
+		if (!this.checkLoggedIn(router) || code === 555) {
 			this.invalidate();
 			swal.fire({
 				icon: "info",
@@ -67,13 +48,13 @@ export default {
 		}
 	},
 	successForgotPassword(swal, code, status) {
-		if (status == true && code == 200) {
+		if (status && code === 200) {
 			swal.fire({
 				icon: "success",
 				title: "Success",
 				text: "Password is successfully sent to your email Id, \nPlease signin to continue.",
 			});
-		} else if (status == true && code == 550) {
+		} else if (status && code === 550) {
 			swal.fire({
 				icon: "error",
 				title: "Profile not found",
